1b8c47b2b556ca9c8a518426c7d1b4f97c58c43f
Sarunrat <tee20122538@gmail.com>
2025-12-23 15:49:10 +0700

    admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Customer,Dress,Rental
 
 # Register your models here.
+
+admin.site.register(Customer)
+admin.site.register(Dress)
+admin.site.register(Rental)
